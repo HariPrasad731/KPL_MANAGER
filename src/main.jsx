3fd0d573e0d461ad16854773da664950945771ec
HariPrasad731 <bodasingihariprasad@thestackly.com>
2025-12-08 11:05:08 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { LeagueProvider } from "./context/LeagueContext";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 import App from "./App";
+import "./index.css";
+
+// your pages
 import Dashboard from "./pages/Dashboard";
 import Teams from "./pages/Teams";
 import TeamDetails from "./pages/TeamDetails";
 import AddTeam from "./pages/AddTeam";
 import Matches from "./pages/Matches";
 import Sponsors from "./pages/Sponsors";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <LeagueProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />}>
-            <Route index element={<Dashboard />} />
-            <Route path="teams" element={<Teams />} />
-            <Route path="teams/:id" element={<TeamDetails />} />
-            <Route path="add-team" element={<AddTeam />} />
-            <Route path="matches" element={<Matches />} />
-            <Route path="sponsors" element={<Sponsors />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </LeagueProvider>
+    {/* VERY IMPORTANT for GitHub Pages */}
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <Routes>
+        {/* App is the layout, Outlet will render children */}
+        <Route path="/" element={<App />}>
+          <Route index element={<Dashboard />} />
+          <Route path="teams" element={<Teams />} />
+          <Route path="teams/:teamId" element={<TeamDetails />} />
+          <Route path="add-team" element={<AddTeam />} />
+          <Route path="matches" element={<Matches />} />
+          <Route path="sponsors" element={<Sponsors />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
