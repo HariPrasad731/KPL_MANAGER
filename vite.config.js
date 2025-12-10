504536b932a3cdc094b4f2f469a316eba25159cb
HariPrasad731 <bodasingihariprasad@thestackly.com>
2025-12-10 15:15:38 +0530

    Modify base path in vite.config.js
    Updated base path for build command in Vite config.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command }) => ({
+export default defineConfig(({ command, mode }) => ({
   plugins: [react()],
-  base: command === 'build' ? '/KPL_MANAGER/' : '/',
+  // base: mode === 'gh' ? './' : '/', 
+   base: command === 'build' ? './' : '/',
 }))
