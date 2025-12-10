fd77aa6d13e96702fe8c507c981c8e6c3c8fb5fd
HariPrasad731 <bodasingihariprasad@thestackly.com>
2025-12-10 15:17:47 +0530

    Update base path configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command, mode }) => ({
   plugins: [react()],
-  // base: mode === 'gh' ? './' : '/', 
-   base: command === 'build' ? './' : '/',
+  base: mode === 'gh' ? './' : '/', 
+   // base: command === 'build' ? './' : '/',
 }))
