c95bf37eefbd990402e14844d951a63796539cf5
HariPrasad731 <bodasingihariprasad@thestackly.com>
2025-12-10 15:57:28 +0530

    Update Vite config by uncommenting and simplifying
    Uncommented the configuration for Vite and removed the base condition based on command.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command, mode }) => ({
+// export default defineConfig(({ command, mode }) => ({
+//   plugins: [react()],
+//   base: mode === 'gh' ? './' : '/', 
+//    // base: command === 'build' ? './' : '/',
+// }))
+
+export default defineConfig(() => ({
   plugins: [react()],
-  base: mode === 'gh' ? './' : '/', 
-   // base: command === 'build' ? './' : '/',
 }))
