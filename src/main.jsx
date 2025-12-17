19d6063fb6007720761605f1898c9f9202596898
HariPrasad731 <bodasingihariprasad@thestackly.com>
2025-12-17 10:28:31 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,44 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  HashRouter,
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-
-import App from "./App";
-import "./index.css";
-
-// context
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { LeagueProvider } from "./context/LeagueContext";
-
-// pages
+import App from "./App";
 import Dashboard from "./pages/Dashboard";
 import Teams from "./pages/Teams";
 import TeamDetails from "./pages/TeamDetails";
 import AddTeam from "./pages/AddTeam";
 import Matches from "./pages/Matches";
 import Sponsors from "./pages/Sponsors";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* 🔹 Wrap everything with LeagueProvider */}
     <LeagueProvider>
-      {/* <HashRouter> */}
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<App />}>
             <Route index element={<Dashboard />} />
             <Route path="teams" element={<Teams />} />
-            <Route path="teams/:teamId" element={<TeamDetails />} />
+            <Route path="teams/:id" element={<TeamDetails />} />
             <Route path="add-team" element={<AddTeam />} />
             <Route path="matches" element={<Matches />} />
             <Route path="sponsors" element={<Sponsors />} />
           </Route>
         </Routes>
-      </BrowserRouter>
-      {/* </HashRouter> */}
+      </HashRouter>
     </LeagueProvider>
   </React.StrictMode>
 );
