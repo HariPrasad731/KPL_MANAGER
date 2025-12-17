d2f94d6e6570aa1eec06aaef243183cb74301662
HariPrasad731 <bodasingihariprasad@thestackly.com>
2025-12-17 10:31:14 +0530

    Refactor Vite config for GitHub Pages
    Updated Vite configuration to support GitHub Pages deployment.

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// export default defineConfig(({ command, mode }) => ({
-//   plugins: [react()],
-//   base: mode === 'gh' ? './' : '/', 
-//    // base: command === 'build' ? './' : '/',
-// }))
-
-export default defineConfig(() => ({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
+  // In dev use "/", in build (for GitHub Pages) use "/KPL_MANAGER/"
+  base: command === 'build' ? '/KPL_MANAGER/' : '/',
 }))
